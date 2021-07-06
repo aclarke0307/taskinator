2fdfe37595c73df0bbbaf54ec95844112fef06c3
aclar <a.clarke0307@gmail.com>
2021-07-05 20:58:08 -0400

    form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,25 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHanderler = function() {
+var createTaskHanderler = function(event) {
+    event.preventDefault();
+var taskNameInput = document.querySelector("input[name='task-name']").value;
+var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+//create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
+
+//create div to hold task info and to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+//add html content to div
+taskInfoEl.innerHTML = "<h3 class ='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+listItemEl.appendChild(taskInfoEl);
+
+//add entire list item to list
+tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHanderler);
+formEl.addEventListener("submit", createTaskHanderler);
